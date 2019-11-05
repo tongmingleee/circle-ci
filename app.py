cf300d9bb7f25d696a89ce7e548ab7c1c31f2d6b
Tongming Li <tongming@getcatalyst.com>
2019-11-05 17:43:13 -0500

    add bandit test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 app = Flask(__name__)
 
-################################################################################################
+
 @app.route('ping', methods=['GET'])
 def ping():
     return Response("pong")
