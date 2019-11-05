a096e199c10ba50b4c8bc7c1e4b6b32c757f6625
Tongming Li <tongming@getcatalyst.com>
2019-11-05 16:25:59 -0500

    update circle ci

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-# Data warehouse integration server
-#
-# Description:
-# Acts as the web entry point to fetch data from warehouses
-#
-# Author:
-# Dylan Watt
-#
-# Notes:
-# All http actions are posts right now as they take sensitive params (credentials), and get params have
-# a habit of ending up in server logs.
-
-import os
-import json
-from flask import Flask, request, jsonify, Response
+from flask import Flask, Response
 import logging
 
 logging.basicConfig()
